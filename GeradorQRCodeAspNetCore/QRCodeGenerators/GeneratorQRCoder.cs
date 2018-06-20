5e6e463e9f62edb4fe9acb84fe5ef351a2c168aa
Érik Thiago <erik.sousa@outlook.com>
2018-06-19 21:49:34 -0300

    Criação das classes responsáveis por gerar os QRCodes a partir de um parâmetro enviado.

---
+++ GeradorQRCodeAspNetCore/QRCodeGenerators/GeneratorQRCoder.cs
@@ -0,0 +1,17 @@
+﻿using QRCoder;
+using System.Drawing;
+
+namespace GeradorQRCodeAspNetCore.QRCodeGenerators
+{
+    public class GeneratorQRCoder
+    {
+        public Bitmap GeneratedQRCodde(string url)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return qrCodeImage;
+        }
+    }
+}
---
+++ GeradorQRCodeAspNetCore/QRCodeGenerators/ZXingGeneratorQRCode.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace GeradorQRCodeAspNetCore.QRCodeGenerators
+{
+    public class ZXingGeneratorQRCode
+    {
+        public Bitmap QRCodeGenerator(int width, int height, string url)
+        {
+            var barCode = new ZXing.BarcodeWriter<ZXing.IBarcodeWriter>
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions() { Width = width, Height = height, Margin = 0 }
+            };
+            return new Bitmap((Image)barCode.Write(url));
+        }
+    }
+}
